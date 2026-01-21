f0c0c0ff9edc2a909bccf8fa6da6747c7f9ca702
BKH <dallas9115@gmail.com>
2026-01-21 19:10:00 +0900

    Feat: product 재고 정보 수 추가

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ public class App {
 
     public static void main(String[] args){
 
-        List<Product> products = new ArrayList<>();
+        List<Product> products = new ArrayList<>(); // Product 클래스 형태에 맞는 객체들만 담는 ArrayList
 
         products.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 10));
         products.add(new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 7));
@@ -16,14 +16,15 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
 
-        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+        DecimalFormat decimalFormat = new DecimalFormat("###,###"); // 가격 단위를 끊어주는 객체
 
         while (true){
             System.out.println("\n [ 실시간 커머스 플랫폼 - 전자제품 ]");
 
             for (int i = 0; i < products.size(); i++) {
                 Product p = products.get(i);
-                System.out.printf("%d. %-15s | %s원 | %s\n", i+1, p.getName(), decimalFormat.format(p.getPrice()),p.getDescription());
+                System.out.printf("%d. %-15s | %s원 | %s | 재고: %d개\n",
+                        i+1, p.getName(), decimalFormat.format(p.getPrice()), p.getDescription(), p.getStock());
 
             }
             System.out.println("0. 종료             | 프로그램 종료 ");
